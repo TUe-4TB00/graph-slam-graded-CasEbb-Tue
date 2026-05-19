4d22ebcfcc2495f79f6492688712069caa582459
Cas Ebberink <c.ebberink@student.tue.nl>
2026-05-19 21:31:22 +0200

    Compute distance by multiplying time and speed instead of addition.

--- src/add_pose.py
+++ src/add_pose.py
@@ -1,16 +1,34 @@
-
 import math
 import numpy as np
 import gtsam
-from gtsam.symbol_shorthand import L, X
+from gtsam.symbol_shorthand import X
 
-PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x, y, theta)
-ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
-MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
+ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(
+    np.array([0.2, 0.2, 0.1])
+)
 
 def add_pose(graph, initial_estimate):
-    # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
+    # Rotate 45° and move ~2m
+    odometry = gtsam.Pose2(
+        2 * math.cos(math.pi / 4),
+        2 * math.sin(math.pi / 4),
+        math.pi / 4
+    )
 
-    # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    
+    # Add odometry factor
+    graph.add(
+        gtsam.BetweenFactorPose2(
+            X(3),
+            X(4),
+            odometry,
+            ODOMETRY_NOISE
+        )
+    )
+
+    # Initial estimate
+    prev_pose = initial_estimate.atPose2(X(3))
+    pose4_guess = prev_pose.compose(odometry)
+
+    initial_estimate.insert(X(4), pose4_guess)
+
     return graph, initial_estimate
